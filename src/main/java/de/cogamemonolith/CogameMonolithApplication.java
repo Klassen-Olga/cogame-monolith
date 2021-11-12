8a16dd3ccc68657804ae661bb9adc4c16623f33e
Klassen-Olga <56605448+Klassen-Olga@users.noreply.github.com>
2021-11-12 21:56:20 +0100

    set up project structure

---
+++ src/main/java/de/cogamemonolith/CogameMonolithApplication.java
@@ -0,0 +1,13 @@
+package de.cogamemonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogameMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogameMonolithApplication.class, args);
+	}
+
+}
